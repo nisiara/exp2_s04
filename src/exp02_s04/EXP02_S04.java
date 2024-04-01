695e6df7f00ee3ae804a4faf0a2dbc0aa9fde451
Nicolas Silva <nic.silvaa@duocuc.cl>
2024-04-01 11:30:48 -0300

    Mejora en validaciones y posición de variables

--- src/exp02_s04/EXP02_S04.java
+++ src/exp02_s04/EXP02_S04.java
@@ -7,6 +7,17 @@ public class EXP02_S04 {
   public static void main(String[] args){
 
     Scanner inputUsuario = new Scanner(System.in);
+    
+    int precioEntrada = 0;
+    int ubicacion = 0;
+    int edad = 0;
+    int tipoPublico = 0;
+    int numeroEntradas = 0;
+    String ubicacionSalida = "";
+    String descuentoSalida;
+    double descuento, precioEntradaPagar;
+    double totalAcumulado = 0;
+      
 
     int opcionMenu;
     System.out.println(" -------------------------------------------");
@@ -18,30 +29,23 @@ public class EXP02_S04 {
     System.out.println("[2] Salir");
     opcionMenu = inputUsuario.nextInt();
 
-    int numeroEntradas = 0;
-    double totalAcumulado = 0;
-
     while (opcionMenu == 1){
-      /*
-      PASO 1:
-      Seleccionar la ubicación de la entrada, la cual tiene asociada un precio
-      */
-      int ubicacion;
-      System.out.println("    Elige el número correspondiente al sector a comprar    ");
-      System.out.println("[1] Palco");
-      System.out.println("[2] Platea Alta");
-      System.out.println("[3] Platea Baja");
-      System.out.println("[4] VIP");
-      ubicacion = inputUsuario.nextInt();
-
-      // Se válida que el número ingresado por el usuario esté dentro del rango propuesto en el menú
-      while (ubicacion == 0 || ubicacion > 4){
-        System.out.println("Debe ingresar un número válido 1-2-3-4");
+     
+      // PASO 1: Seleccionar la ubicación de la entrada, la cual tiene asociada un precio
+      do {
+        System.out.println("Elige el número correspondiente al sector a comprar");
+        System.out.println("[1] Palco");
+        System.out.println("[2] Platea Alta");
+        System.out.println("[3] Platea Baja");
+        System.out.println("[4] VIP");
         ubicacion = inputUsuario.nextInt();
-      }
-
-      int precioEntrada = 0;
-      String ubicacionSalida = "";
+        
+        if(ubicacion <= 0 || ubicacion > 4){
+          System.out.println("Ingresaste un valor incorrecto");
+        }
+      } while (ubicacion <= 0 || ubicacion > 4);
+      
+      
       switch (ubicacion){
         case 1:
           ubicacionSalida = "Palco";
@@ -61,40 +65,42 @@ public class EXP02_S04 {
           break;
       }
 
-      /*
-      PASO 2
-      Se pregunta por la edad del usuario, ya que dependiendo de su edad, se puede aplicar un descuento
-      */
-      int edad;
-      System.out.println("Ingresa la edad del comprador");
-      edad = inputUsuario.nextInt();
-
-      while( edad < 15 ){
-        System.out.println("La obra de teatro es para mayores de 14 años");
+      System.out.println("\nHas seleccionado la ubicación: " + ubicacionSalida);
+      
+      // PASO 2: Se pregunta por la edad del usuario. Dependiendo de su edad, se puede aplicar un descuento.
+      do{
+        System.out.println("Ingresa la edad del comprador");
         edad = inputUsuario.nextInt();
-      }
+      
+        if(edad < 15){
+          System.out.println("La obra de teatro es para mayores de 14 años");
+        }
+      } while(edad < 15);
+      
+
       /*
       Si el usuario es Tercera Edad se le aplicará un descuento del 15% sobre el valor de la entrada.
       Ya que es el descuento más alto que se puede alcanzar, no se validará si este usuario es estudiante (10% de descuento).
       */
-      double descuento, precioEntradaPagar;
-      String descuentoSalida;
       if( edad >= 60){
         descuento = 0.85;
         precioEntradaPagar = precioEntrada * descuento;
         descuentoSalida = "Tienes un descuento del 15% (Tercera edad)";
       }
       else {
-        int tipoPublico;
-        System.out.println("Es estudiante?");
-        System.out.println("[1]Si [2]No");
-        tipoPublico = inputUsuario.nextInt();
-
+        
         // Se valida que el número ingresado por el usuario esté dentro de las opciones presentadas.
-        while(tipoPublico != 1 && tipoPublico != 2){
-          System.out.println("Debe ingresar un número válido [1]Si - [2]No");
+        do{
+          System.out.println("¿El comprador es estudiante?");
+          System.out.println("[1]Si [2]No");
           tipoPublico = inputUsuario.nextInt();
-        }
+          
+          if( tipoPublico != 1 && tipoPublico != 2){
+            System.out.println("Ingresaste un valor incorrecto.");
+          }
+          
+        } while(tipoPublico != 1 && tipoPublico != 2);
+        
 
         // Se aplicará un 10% de descuento si el tipo de píblico es estudiante, de lo contrario ningún descuento se aplicará
         if ( tipoPublico == 1 ) {
@@ -109,17 +115,14 @@ public class EXP02_S04 {
         }
       }
 
-      // Ya que tenemos toda la información, podemos hacer el resumen de la compra
-      System.out.println("Resumen entrada: N˚" + (numeroEntradas += 1) );
+      numeroEntradas += 1;
+      
+      // PASO 3: Mostrar el resumen de la entrada
+      System.out.println("Resumen entrada: N˚" + numeroEntradas );
       System.out.println("Ubicación: " + ubicacionSalida);
       System.out.println("Precio entrada: $" + precioEntrada);
       System.out.println("Descuento: " + descuentoSalida);
-      if (precioEntradaPagar == 0){
-        System.out.println("Total a pagar por la entrada: $" + precioEntrada);
-      } else {
-        System.out.println("Total a pagar por la entrada: $" + precioEntradaPagar);
-      }
-      System.out.println();
+      System.out.println("Total a pagar por la entrada: $" + precioEntradaPagar + "\n");
 
       totalAcumulado += precioEntradaPagar;
 
